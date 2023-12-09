4522933467581b2aedaf5f675e1b9bf1d2108bd7
phamngophat <ngo11062005@gmail.com>
2023-12-09 18:09:39 +0700

    update-code

---
+++ Untitled-1.c
@@ -0,0 +1,5 @@
+#includen<stdio.h>
+int main (){
+    printf("hello");
+    return 0;
+}
---
+++ Untitled1.cpp
@@ -0,0 +1,7 @@
+/* dung ham printf de in ra dong hello world */
+#include<stdio.h>
+int main() {
+	// dung ham printf de in ramang hinh 
+	printf(" hello ngo phat dep trai");
+	return 0 ;
+}
